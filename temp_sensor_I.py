5790a90bf066ef8d79d35d0416d2152c831aa465
tobegram <tom_beyer@gmx.net>
2022-01-30 21:39:19 +0100

    Initialize on GitHub

--- temp_sensor_I.py
+++ temp_sensor_I.py
@@ -1,3 +1,5 @@
+from base64 import encode
+from encodings import utf_8
 from fileinput import close
 import serial
 import time
@@ -9,9 +11,7 @@ data_file = 'data.csv'
 # Initialisieren der Daten-Liste und schreiben des Tabellenkopfes in die csv Datei
 data_list = ["Datum", "Temperatur [°C]", "Feuchte [%]"]
 
-with open(data_file, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(data_list)
+
 
 # Arduino Objekt wird erzeugt
 # Print Statement zur Kontrolle des Kommunikation zum Arduino
@@ -21,23 +21,26 @@ print("Loop startet")
 
 # Schleife für ankommendes Datenpaket
 for i in range(6):
-    list_a = []
-    list_abc = []
+    list_a = []  # Liste durchläuft 2x loop, um Temp und Feuchte zu speichern
+    
     list_a.append(time.ctime())
     for i in range(2):
         buffer = board.readline()
         data_raw = buffer.decode()
         data_clean = data_raw[:-3]
         list_a.append(data_clean)
-    list_abc.extend(list_a)
-    print(list_abc)
-    data_list.extend(list_abc)
+    
+    data_list.append(list_a)
     print(data_list)
+    
             
-    """
-    with open(data_file, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(data_list)"""
+    
+    
+    with open(data_file, 'w') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(data_list)
+    
+    file.close
 
 print("Loop endet")
 
