6cc0dc1587e3f41e8c5a2712960414f72abb31d6
Tom Beyer <tom_beyer@gmx.net>
2022-02-07 13:39:19 +0100

    first diagramm

--- temp_hum_sensoring_DHT11/data_analysis.py
+++ temp_hum_sensoring_DHT11/data_analysis.py
@@ -1,12 +1,27 @@
-import pandas as pd
+from shutil import which
+import matplotlib.pyplot as plt
 import csv
 
 filename = 'temp_hum_sensoring_DHT11/data.csv'
 with open(filename) as file:
     reader = csv.reader(file)
     header_row = next(reader)
-    print(header_row)
+  
+    temps = []
+    for row in reader:
+        temp = float(row[1])
+        temps.append(temp)
+        
+       
 
-data = pd.read_csv("data.csv", sep=',')
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(temps, c='red')
 
-print(data) 
+# format the diagramm
+plt.title("Temperaturkurve vom DHT11", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperatur (°C)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
