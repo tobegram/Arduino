1007d0c9aa5b0a3760d517bc2dda1153ac34b1ab
Tom Beyer <tom_beyer@gmx.net>
2022-02-15 19:36:56 +0100

    OLED with temp running

--- sensoring_temp_DS18B20/ard_sketch/sketch_feb03a/sketch_feb03a.ino
+++ sensoring_temp_DS18B20/ard_sketch/sketch_feb03a/sketch_feb03a.ino
@@ -1,37 +1,46 @@
-/* DS18B20 1-Wire digital temperature sensor with Arduino example code. More info: https://www.makerguides.com */
-
-// Include the required Arduino libraries:
+#include <GyverOLED.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Define to which pin of the Arduino the 1-Wire bus is connected:
-#define ONE_WIRE_BUS 2
+#define SENSOR_PIN 2 // Arduino pin connected to DS18B20 sensor's DQ pin
 
-// Create a new instance of the oneWire class to communicate with any OneWire device:
-OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass the oneWire reference to DallasTemperature library:
-DallasTemperature sensors(&oneWire);
+GyverOLED<SSH1106_128x64> oled;
+OneWire oneWire(SENSOR_PIN);         // setup a oneWire instance
+DallasTemperature tempSensor(&oneWire); // pass oneWire to DallasTemperature library
+
+String tempString;
 
 void setup() {
-  // Begin serial communication at a baud rate of 9600:
   Serial.begin(9600);
-  // Start up the library:
-  sensors.begin();
+  oled.init();            // initialize the OLED
+  tempSensor.begin();     // initialize the sensor
+  tempString.reserve(10); // to avoid fragmenting memory when using String
+
+  delay(2000);         // wait for initializing
 }
 
 void loop() {
-  // Send the command for all devices on the bus to perform a temperature conversion:
-  sensors.requestTemperatures();
-
-  // Fetch the temperature in degrees Celsius for device index:
-  float tempC = sensors.getTempCByIndex(0); // the index 0 refers to the first device
+  tempSensor.requestTemperatures();             // send the command to get temperatures
+  float tempCelsius = tempSensor.getTempCByIndex(0);  // read temperature in Celsius
+  tempString  = String(tempCelsius, 2); // two decimal places
+  Serial.println(tempString); // print the temperature in Celsius to Serial Monitor
+  
+  
+  oled.clear();
+  oled.home();
+  oled.setScale(3);
+  oled.setCursor(0, 3);
+  oled.print(tempCelsius);
+  oled.circle(100, 27, 3);
+  oled.setScale(3);
+  oled.setCursor(108, 3);
+  oled.print("C");
+  oled.update();
   
 
-  Serial.println(tempC);
-  
-
-
-  // Wait 1 second:
   delay(1000);
 }
